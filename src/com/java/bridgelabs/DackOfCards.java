b4e82e7dc89cdd57ffcb5558a33f0e885fec74b4
praju <sawantprajakta84@gmail.com>
2022-03-30 20:11:17 +0530

    Added Deck of Cards Program

--- src/com/java/bridgelabs/DackOfCards.java
+++
@@ -1,9 +0,0 @@
-package com.java.bridgelabs;
-
-public class DackOfCards
-{
-    public static void main(String[] args) {
-        System.out.println("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-Dack Of Cards *-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
-
-    }
-}
