a695d75abc147152b0b79d30d7dc5e9ddad87539
praju <sawantprajakta84@gmail.com>
2022-03-30 08:36:05 +0530

    Welcome to Dack of cards

---
+++ src/com/java/bridgelabs/DackOfCards.java
@@ -0,0 +1,9 @@
+package com.java.bridgelabs;
+
+public class DackOfCards
+{
+    public static void main(String[] args) {
+        System.out.println("*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-Dack Of Cards *-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+
+    }
+}
